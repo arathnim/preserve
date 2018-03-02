9a627b0916bd42f86b40205c5ac1adb96c742fb9
arathnim <arathnim@gmail.com>
2018-03-01 21:36:10 -0600

    more tab problems

--- preserve.lisp
+++ preserve.lisp
@@ -4,24 +4,24 @@
 
 (defpackage #:preserve
    (:use cl cl-store iterate alexandria)
-	(:export #:preserve #:preserve-end))
+   (:export #:preserve #:preserve-end))
 
 (in-package preserve)
 
 (defvar *varlist*)
 
 (defmacro preserve (&rest vars)
-	(setf *varlist* (mapcar #'second vars))
-	(with-gensyms (i r)
-		`(let ((,i (open ".preserve" :direction :probe :if-does-not-exist nil)))
-		    (if ,i
-				  (let ((,r (restore ".preserve")))
-				     (progn 
-						   ,@(iter (for v in vars) 
-									  (for n upfrom 0) 
-									  (collect (list (first v) (second v) `(nth ,n ,r))))))
-				  (progn ,@vars)))))
+   (setf *varlist* (mapcar #'second vars))
+   (with-gensyms (i r)
+      `(let ((,i (open ".preserve" :direction :probe :if-does-not-exist nil)))
+          (if ,i
+              (let ((,r (restore ".preserve")))
+                 (progn 
+                     ,@(iter (for v in vars) 
+                             (for n upfrom 0) 
+                             (collect (list (first v) (second v) `(nth ,n ,r))))))
+              (progn ,@vars)))))
 
 (defmacro preserve-end ()
-	`(store (list ,@*varlist*) ".preserve"))
+   `(store (list ,@*varlist*) ".preserve"))
 
